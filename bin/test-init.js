f0a5a6b44b33abcf69fb495713b3142afe00dd5d
DangoSky <1341514988@qq.com>
2020-09-11 18:14:57 +0800

    feat: support download templates
    Change-Id: Iee8064a77124f4b2fa87e1b57d7c46816d2f92cc

--- bin/test-init.js
+++ bin/test-init.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 const program = require('commander')
-
-console.log(11);
+const download = require('../lib/download.js')
+const path = require('path');
 
 program.parse(process.argv);
 
@@ -12,6 +12,17 @@ console.log(projectName);
 
 if (!projectName) {
   // 相当于执行命令的--help选项，显示help信息，这是commander内置的一个命令选项
-  program.help() 
+  program.help()
   return
 }
+
+console.log(`Start downloading templates for ${projectName}`);
+
+
+download(projectName)
+  .then(targetPath => {
+    console.log(`Success! Created ${projectName} at ${targetPath}`);
+  })
+  .catch(err => {
+    console.log(err);
+  })
